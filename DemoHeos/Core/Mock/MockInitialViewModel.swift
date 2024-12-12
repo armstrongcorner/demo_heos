5cf5c0a7c926013c295778c123d9244ee60b2417
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2024-12-12 23:33:08 +0800

    Update MockInitialViewModel.swift

--- DemoHeos/Core/Mock/MockInitialViewModel.swift
+++ DemoHeos/Core/Mock/MockInitialViewModel.swift
@@ -41,7 +41,6 @@ class MockInitialViewModel: InitialViewModelProtocol {
                 fetchDataState = .error
                 errorMessage = "Mock error occurred"
             } else {
-                print("aaaaaa")
                 if shouldUseMockData {
                     // Mock load from local file
                     devices = [mockDevice1, mockDevice2, mockDevice3]
