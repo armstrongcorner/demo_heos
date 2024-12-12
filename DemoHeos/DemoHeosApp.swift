043d43413a08d197eccd667a11c152f0245176a5
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2024-12-12 18:25:00 +0800

    Move shareVM and playVM injection from MainScreen to App starter

--- DemoHeos/DemoHeosApp.swift
+++ DemoHeos/DemoHeosApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct DemoHeosApp: App {
+    @State private var shareVM: ShareViewModelProtocol
+    @State private var playVM: PlayViewModelProtocol
+    
+    init() {
+        if ProcessInfo.processInfo.arguments.contains("-isUITesting") {
+            self.shareVM = MockShareViewModel(isMock: false)
+            self.playVM = PlayViewModel(
+                devices: [mockDevice1, mockDevice2, mockDevice3],
+                selectedDevice: mockDevice1,
+                selectedPlayingItem: mockNowPlayingItem1
+            )
+        } else {
+            self.shareVM = ShareViewModel(selectedTab: .room, refreshData: true)
+            self.playVM = PlayViewModel()
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             MainScreen()
+                .environment(\.shareViewModel, shareVM)
+                .environment(\.playViewModel, playVM)
         }
     }
 }
